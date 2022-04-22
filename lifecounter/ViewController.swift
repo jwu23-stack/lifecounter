267a142543b813b3929c31881f909c1b5e4188a5
jwu23 <jwu23@uw.edu>
2022-04-21 20:02:16 -0700

    finished assignment

--- lifecounter/ViewController.swift
+++ lifecounter/ViewController.swift
@@ -8,12 +8,65 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var player1Lives : Int = 20
+    var player2Lives : Int = 20
+    
+    //Player 1
+    @IBOutlet weak var player1LivesLabel: UILabel!
+    @IBOutlet weak var player1Plus1: UIButton!
+    @IBOutlet weak var player1Plus5: UIButton!
+    @IBOutlet weak var player1Minus1: UIButton!
+    @IBOutlet weak var player1Minus5: UIButton!
+    
+    //Player 2
+    @IBOutlet weak var player2LivesLabel: UILabel!
+    @IBOutlet weak var player2Plus1: UIButton!
+    @IBOutlet weak var player2Plus5: UIButton!
+    @IBOutlet weak var player2Minus1: UIButton!
+    @IBOutlet weak var player2Minus5: UIButton!
+    
+    // Losing player label
+    @IBOutlet weak var losingPlayer: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func updateScore(_ sender: UIButton) {
+        switch sender {
+        case player1Plus1:
+            player1Lives += 1
+        case player1Plus5:
+            player1Lives += 5
+        case player1Minus1:
+            player1Lives -= 1
+        case player1Minus5:
+            player1Lives -= 5
+        case player2Plus1:
+            player2Lives += 1
+        case player2Plus5:
+            player2Lives += 5
+        case player2Minus1:
+            player2Lives -= 1
+        case player2Minus5:
+            player2Lives -= 5
+        default:
+            print(sender)
+        }
+        updateLivesCounter()
+    }
+    
+    func updateLivesCounter() -> Void {
+        player1LivesLabel.text = "\(player1Lives)"
+        player2LivesLabel.text = "\(player2Lives)"
+        if player1Lives <= 0 {
+            losingPlayer.text = "Player 1 LOSES!"
+        }
+        if player2Lives <= 0 {
+            losingPlayer.text = "Player 2 LOSES!"
+        }
+    }
+    
 }
 
